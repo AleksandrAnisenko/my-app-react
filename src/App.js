746b93bba0240b6dc3aacf9ed59fc921fc7fc053
Aleksandr Anisenko <aleksandr.anisenko@mail.ru>
2021-01-20 00:21:07 +0300

    Deployed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import './App.css';
 import UserList from './UserList';
 
+
 function App() {
   return (
-    <div>
       <UserList/>
-      <Route exact path={`/`} render={ (routerProps) => < Home routerProps={routerProps} setUpGame={this.setUpGame} />} />
-    </div>
   );
 }
 
